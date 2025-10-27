f4d7d83cb27bbc2aba39324c92e94fe3e8656b4e
Antony <antonymurithi51@gmail.com>
2025-10-27 21:00:48 +0300

    created the CORS middleware to connect React frontend

---
+++ backend/utils/cors.go
@@ -0,0 +1,16 @@
+package utils
+
+import "github.com/gin-gonic/gin"
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headfers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
