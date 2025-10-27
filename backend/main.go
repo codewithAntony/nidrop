e142ead76598911662437271675da3d8f018c709
Antony <antonymurithi51@gmail.com>
2025-10-27 20:19:29 +0300

    Installed the dependacies needed

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
+func main() {
+	godotenv.Load()
+
+	r := gin.Default()
+	r.Use(utils.CORSMiddleware())
+
+	routes.RegisterRoutes(r)
+
+	r.Run(":8080")
 }
